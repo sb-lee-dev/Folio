54b01dd21b1ea1e78126cd48dfc7adff5b40ed88
Shu Ma Wa Win Htun <smwwin-htun+seneca@myseneca.ca>
2026-04-25 19:18:58 -0400

    Completed Navbar component with routing and styling and fixed linking path

--- DevDeck/src/App.jsx
+++ DevDeck/src/App.jsx
@@ -1,16 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 import ProfilePage from "./pages/profile/ProfilePage";
 import ProjectsPage from "./pages/projects/ProjectsPage";
-import PublicPage from "./pages/public/PublicPage";
 import RoadmapPage from "./pages/roadmap/RoadmapPage";
+import PublicPage from "./pages/public/PublicPage";
 
 function App() {
   return (
     <div className="app">
+      <Navbar /> 
       <Routes>
-        <Route path="/" element={<PublicPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/public" element={<PublicPage />} />
+        <Route path="/" element={<ProfilePage />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/roadmap" element={<RoadmapPage />} />
       </Routes>
--- DevDeck/src/components/Navbar.jsx
+++ DevDeck/src/components/Navbar.jsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router";
 import "./Navbar.css";
 
 export default function NavBar() {
   return (
     <>
-      <p>this is navbar</p>
+    <div className="nav-bar">
+      <div className="right-section">
+        <span className="logo">Folio</span>
+      </div>
+      <div className="left-section">
+        <Link className='nav-link' to="/">Profile</Link>
+        <Link className='nav-link' to="/projects">Projects</Link>
+        <Link className='nav-link' to="/roadmap">Roadmap</Link>
+        <Link className='nav-link' to="/public">Public</Link>
+      </div>
+    </div>
     </>
   );
 }
