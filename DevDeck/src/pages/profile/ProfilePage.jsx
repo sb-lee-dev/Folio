860f0b551596d30969d699c20fae2c2f2556c9de
Shu Ma Wa Win Htun <smwwin-htun+seneca@myseneca.ca>
2026-04-26 14:09:51 -0400

    Worked on the profile section of the profile page

--- DevDeck/src/pages/profile/ProfilePage.jsx
+++ DevDeck/src/pages/profile/ProfilePage.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
+import { users } from "../../data";
+import "./ProfilePage.css";
 
+function getInitials(name) {
+  return name
+    .split(" ")
+    .slice(0, 2)
+    .map((word) => word[0])
+    .join("");
+}
 
 export default function ProfilePage() {
+  const [profileData] = useState(users[1].profile);
+
   return (
-    <>
-      <div className='user-info'>
-
+    <div className="profile-page">
+      <div className="profile-section">
+        <div className="avatar">{getInitials(profileData.name)}</div>
+        <div className="profile-info">
+          <h3>{profileData.name}</h3>
+          <p>{profileData.school}</p>
+          <p>{profileData.bio}</p>
+          <div className="links">
+            <a href={profileData.github} target="_blank">
+              GitHub
+            </a>
+            <a href={profileData.linkedin} target="_blank">
+              LinkedIn
+            </a>
+          </div>
+        </div>
       </div>
-      <div className='skills-section'>
-
-      </div>
-    </>
+    </div>
   );
 }
