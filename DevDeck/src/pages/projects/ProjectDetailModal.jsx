902bac123d15caa51fba85031cc95e2275f847c9
Seongbae Lee <slee617seneca@gmail.com>
2026-04-28 17:26:48 +0900

    Separate details modal into displayModal and editModal

--- DevDeck/src/pages/projects/ProjectDetailModal.jsx
+++ DevDeck/src/pages/projects/ProjectDetailModal.jsx
@@ -1,6 +1,6 @@
-import axios from "axios";
-import { Fragment, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import ProjectEditModal from "./ProjectEditModal";
+import ProjectDisplayModal from "./ProjectDisplayModal";
 
 export default function ProjectDetailModal({
   user,
@@ -9,45 +9,7 @@ export default function ProjectDetailModal({
   setCurrentProject,
   setIsDetailModalOpen,
 }) {
-  const { userId } = useParams();
-  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
   const [isEditing, setIsEditing] = useState(false);
-  const [tempTags, setTempTags] = useState([...currentProject.tags]);
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-
-    setCurrentProject({
-      ...currentProject,
-      [name]: value,
-    });
-  };
-
-  const handleTagsChange = (e) => {
-    setTempTags(e.target.value);
-  };
-
-  const modifyProject = async () => {
-    const updatedUser = {
-      ...user,
-      projects: user.projects.map((project) =>
-        project.id === currentProject.id
-          ? {
-              ...currentProject,
-              tags: tempTags.split(",").map((tag) => tag.trim()),
-            }
-          : project,
-      ),
-    };
-
-    await axios.put(`${API_BASE_URL}/users/${userId}`, updatedUser);
-    setUser(updatedUser);
-    setCurrentProject({
-      ...currentProject,
-      tags: tempTags.split(",").map((tag) => tag.trim()),
-    });
-    setIsEditing(false);
-  };
 
   const closeModal = () => {
     setIsDetailModalOpen(false);
@@ -57,77 +19,21 @@ export default function ProjectDetailModal({
     <div className="modal-bg">
       <div className="modal-box">
         {isEditing && (
-          <Fragment>
-            <h2>Add Project</h2>
-
-            <div style={{ display: "flex", gap: "10px" }}>
-              <input
-                style={{ flexGrow: 1 }}
-                name="title"
-                placeholder="Project title"
-                value={currentProject.title}
-                onChange={handleChange}
-              />
-              <select
-                name="progress"
-                value={currentProject.progress}
-                onChange={handleChange}
-              >
-                <option>In progress</option>
-                <option>Completed</option>
-                <option>Planning</option>
-              </select>
-            </div>
-
-            <textarea
-              name="description"
-              placeholder="Project description"
-              value={currentProject.description}
-              onChange={handleChange}
-            />
-
-            <input
-              name="tags"
-              placeholder="Tags: React, CSS, API"
-              value={tempTags}
-              onChange={handleTagsChange}
-            />
-
-            <div className="modal-buttons">
-              <button onClick={modifyProject}>Edit</button>
-              <button onClick={() => setIsEditing(false)}>Cancel</button>
-            </div>
-          </Fragment>
+          <ProjectEditModal
+            user={user}
+            setUser={setUser}
+            currentProject={currentProject}
+            setCurrentProject={setCurrentProject}
+            setIsEditing={setIsEditing}
+          />
         )}
 
         {!isEditing && (
-          <Fragment>
-            <h2>Project Detail</h2>
-
-            <div className="modal-project-header">
-              <h3 className="modal-project-title">{currentProject.title}</h3>
-              <span
-                className="project-header-progress"
-                style={{ alignSelf: "flex-start" }}
-              >
-                {currentProject.progress}
-              </span>
-            </div>
-
-            <p className="modal-desc">{currentProject.description}</p>
-
-            <div className="project-tags project-tags-underline">
-              {currentProject &&
-                currentProject.tags.map((tag) => {
-                  return <div className="project-tag">{tag}</div>;
-                })}
-            </div>
-
-            <div className="modal-buttons">
-              <button onClick={() => setIsEditing(true)}>Edit</button>
-              <button onClick={closeModal}>Close</button>
-            </div>
-          </Fragment>
+          <ProjectDisplayModal
+            closeModal={closeModal}
+            currentProject={currentProject}
+            setIsEditing={setIsEditing}
+          />
         )}
       </div>
     </div>
---
+++ DevDeck/src/pages/projects/ProjectDisplayModal.jsx
@@ -0,0 +1,37 @@
+import { Fragment } from "react";
+
+export default function ProjectDisplayModal({
+  currentProject,
+  setIsEditing,
+  closeModal,
+}) {
+  return (
+    <Fragment>
+      <h2>Project Detail</h2>
+
+      <div className="modal-project-header">
+        <h3 className="modal-project-title">{currentProject.title}</h3>
+        <span
+          className="project-header-progress"
+          style={{ alignSelf: "flex-start" }}
+        >
+          {currentProject.progress}
+        </span>
+      </div>
+
+      <p className="modal-desc">{currentProject.description}</p>
+
+      <div className="project-tags project-tags-underline">
+        {currentProject &&
+          currentProject.tags.map((tag) => {
+            return <div className="project-tag">{tag}</div>;
+          })}
+      </div>
+
+      <div className="modal-buttons">
+        <button onClick={() => setIsEditing(true)}>Edit</button>
+        <button onClick={closeModal}>Close</button>
+      </div>
+    </Fragment>
+  );
+}
