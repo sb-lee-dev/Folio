154e1c01508072efab57e667b0c8ec1cacd05f0c
Shu Ma Wa Win Htun <smwwin-htun+seneca@myseneca.ca>
2026-04-29 19:56:38 -0400

    Worked on the profile section of public page

--- DevDeck/src/pages/public/PublicPage.jsx
+++ DevDeck/src/pages/public/PublicPage.jsx
@@ -1,7 +1,47 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getInitials } from "../../utils/getInitials";
+import axios from "axios";
+import "./PublicPage.css";
+
 export default function PublicPage() {
+  const { userId } = useParams();
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const [userData, setUserData] = useState({
+    profile: {},
+    projects: [],
+    skills: [],
+    roadmap: [],
+  });
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const response = await axios.get(`${API_BASE_URL}/users/${userId}`);
+      setUserData(response.data);
+    };
+    getUserData();
+  }, [userId]);
+
   return (
-    <>
-      <p>this is PublicPage</p>
-    </>
+    <div className="profile-container">
+      <div className="icon">
+        {userData.profile.name
+          ? getInitials(userData.profile.name)
+          : getInitials("")}
+      </div>
+      <div className="profile-details">
+        <h3 style={{ color: '#3c3489' }}>{userData.profile.name}</h3>
+        <p>{userData.profile.school}</p>
+        <p>{userData.profile.bio}</p>
+        <div className="profile-links">
+          <a href={userData.profile.github} target="_blank">
+            GitHub
+          </a>
+          <a href={userData.profile.linkedin} target="_blank">
+            LinkedIn
+          </a>
+        </div>
+      </div>
+    </div>
   );
 }
