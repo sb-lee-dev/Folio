e6f5cc7eb3cb9c3d9602f15c74845205be3e7dee
Seongbae Lee <slee617seneca@gmail.com>
2026-04-30 10:05:22 +0900

    Update navbar and user select page
    1. Folio logo directs to the profile page
    2. When a user is selected, it directs to the profile page
    3. User data on select page is based on mockAPI.
    4. Add user avatar on navbar.

--- DevDeck/src/components/Navbar.jsx
+++ DevDeck/src/components/Navbar.jsx
@@ -1,18 +1,34 @@
-import { Link, useParams } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 import "./Navbar.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function NavBar() {
   const { userId } = useParams();
+  const [user, setUser] = useState(null);
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await axios.get(`${API_BASE_URL}/users/${userId}`);
+      setUser(response.data);
+    };
+
+    getUser();
+  }, [userId]);
+
+  console.log(user);
 
   return (
     <>
       <div className="nav-bar">
-        <div className="right-section">
+        <div className="left-section">
           <span className="logo">
-            <Link to="/profile">Folio</Link>
+            <Link to={`/profile/${userId}`}>Folio</Link>
           </span>
         </div>
-        <div className="left-section">
+        <div className="right-section">
           <Link className="nav-link" to={`/profile/${userId}`}>
             Profile
           </Link>
@@ -25,6 +41,11 @@ export default function NavBar() {
           <Link className="nav-link" to={`/public/${userId}`}>
             Public
           </Link>
+          {user ? (
+            <div className="nav-avatar" onClick={() => navigate("/")}>
+              {user.initials}
+            </div>
+          ) : null}
         </div>
       </div>
     </>
--- DevDeck/src/pages/user-select/UserSelectorPage.jsx
+++ DevDeck/src/pages/user-select/UserSelectorPage.jsx
@@ -1,21 +1,22 @@
 // SelectUser.jsx
 import { Link } from "react-router-dom";
 import "./UserSelectorPage.css";
-
-const users = [
-  {
-    id: 1,
-    name: "Shu Ma Wa Win Htun",
-    initials: "SM",
-  },
-  {
-    id: 2,
-    name: "Seongbae Lee",
-    initials: "SL",
-  },
-];
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function UserSelector() {
+  const [users, setUsers] = useState(null);
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await axios.get(`${API_BASE_URL}/users`);
+      setUsers(response.data);
+    };
+
+    getUser();
+  }, []);
+
   return (
     <div className="select-page">
       <span className="select-logo">Folio</span>
@@ -25,20 +26,21 @@ export default function UserSelector() {
         <p>Select a user to view their portfolio.</p>
 
         <div className="user-list">
-          {users.map((user) => (
-            <Link
-              to={`/roadmap/${user.id}`}
-              className="user-card"
-              key={user.id}
-            >
-              <div className="avatar">{user.initials}</div>
+          {users &&
+            users.map((user) => (
+              <Link
+                to={`/profile/${user.id}`}
+                className="user-card"
+                key={user.id}
+              >
+                <div className="avatar">{user.initials}</div>
 
-              <div>
-                <h3>{user.name}</h3>
-                <p>{user.role}</p>
-              </div>
-            </Link>
-          ))}
+                <div>
+                  <h3>{user.name}</h3>
+                  <p>{user.role}</p>
+                </div>
+              </Link>
+            ))}
         </div>
       </div>
     </div>
